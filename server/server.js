56e236b4982dae7b34f97cc9f2efdcd726c19fdc
Cassandra Kornhiser <cvkornhiser@gmail.com>
2021-03-11 11:39:00 -0500

    Added browser icon

--- server/server.js
+++ server/server.js
@@ -27,8 +27,11 @@ mongoose
 const server = app.listen(port, () =>
   console.log(`Server running on port ${port}`)
 );
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
 }
 process.on("unhandledRejection", (err) => {
   console.log("UNHANDLED REJECTION! 💥 Shutting down...");
